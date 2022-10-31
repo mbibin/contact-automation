15b780205514e14e317d69b4411b34dad1a8cc6c
mbibin <bibinphmathew@gmail.com>
2022-11-01 01:49:17 +0530

    edge case tests

--- test_contact_form.py
+++ test_contact_form.py
@@ -1,8 +1,11 @@
+import time
 import unittest
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class ContactFormTest(unittest.TestCase):
@@ -23,9 +26,31 @@ class ContactFormTest(unittest.TestCase):
         self.driver.find_element(By.ID, "message").send_keys("Testing")
         self.driver.find_element(By.ID, "contactButton").click()
 
-        print("Contact submitted successfully")
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.text_to_be_present_in_element((By.ID, 'formMessage'), "Message sent successfully!"))
 
+    def test_invalid_form_inputs(self):
+        self.driver.get(self.LINK)
+        self.driver.maximize_window()
+        self.driver.find_element(By.NAME, "email").send_keys("test.com")
+        self.driver.find_element(By.ID, "phone").send_keys("01010101")
 
+        self.driver.find_element(By.ID, "contactButton").click()
 
+        wait = WebDriverWait(self.driver, 2)
+        wait.until(EC.text_to_be_present_in_element(
+            (By.XPATH, "//input[@name='fullname']/following-sibling::div"), "Field required")
+        )
+        wait.until(EC.text_to_be_present_in_element(
+            (By.XPATH, "//input[@name='email']/following-sibling::div"), "Field required")
+        )
+
+        wait.until(EC.text_to_be_present_in_element(
+            (By.XPATH, "//input[@name='phone']/following-sibling::div"), "Field required")
+        )
+
+        wait.until(EC.text_to_be_present_in_element(
+            (By.XPATH, "//textarea[@name='message']/following-sibling::div"), "Field required")
+        )
 
 
